5e25233427ce57795330149da8f9a244547c0092
zineb bennani <140564571+zinebni@users.noreply.github.com>
2026-01-22 20:56:09 +0100

    socketUrl not wsUrl in environment .prod.ts

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://64.226.94.118:5000/api',  // ton IP publique
-  wsUrl: 'ws://64.226.94.118:5000'     // pour Socket.io
+  apiUrl: 'http://64.226.94.118:5000/api',
+  socketUrl: 'http://64.226.94.118:5000'
 };
